4813a6c76fd1fd586238cfad0806dd73e0514710
gsheyn05 <58836682+gsheyn05@users.noreply.github.com>
2022-05-26 18:45:43 -0400

    Added Github Logo and HomePage Wave

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,23 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Navbar} from "../Components/Navbar";
 import { Layout } from '../Components/Layout';
+import { Container } from '../Components/container';
 
 const Home: NextPage = () => {
   return (
     <Layout>
     <Navbar/>
+    
+    <Container>
+    <header>
+      <h1 className="flex text-3xl font-bold">
+        Gregory Sheyn <span className="animate-[wave_5s_ease-in-out_2] hover:animate-[wave_3s]">👋🏻</span>
+        
+        </h1>
+    </header>
+
+    </Container>
+
     </Layout>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,17 @@ module.exports = {
     extend: {
       keyframes:{
         wave:{
-        to:{
-          "margin-left":"-51%"
-        }
+        
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+          
+        
       }
 
       }
