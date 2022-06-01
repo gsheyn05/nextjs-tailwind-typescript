8b6364978568e27da0ac19790ed056e556a2c9ff
gsheyn05 <58836682+gsheyn05@users.noreply.github.com>
2022-06-01 14:08:13 -0400

    Updated the spacing of Navbar
    Messed around with tailwindcss to center the navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -5,6 +5,7 @@ import { useTheme } from 'next-themes';
 import {
     MoonIcon,
     SunIcon} from '@heroicons/react/outline';
+import { Container } from './container';
  
 
 export const Navbar : React.FC = () =>{
@@ -12,29 +13,32 @@ export const Navbar : React.FC = () =>{
     const router = useRouter();
 
     return(
+      <Container>
 <nav className="flex flex-row items-center justify-center top-background sticky w-full h-20 z-50 top-0 mb-8 backdrop-blur-2xl">
       
-      <div className="container mx-auto px-5 max-w-5xl flex flex-row justify-start">
+      <div className="container mx-auto  max-w-5xl flex flex-row justify-start">
         <div className="flex flex-row space-x-7">
-   
+        <div>
         <Link href="/">
         <a className={`transition mr-4 font-bold  my-auto hover:text-blue-600 hover:-translate-y-1 ${router.pathname == "/" ? "text-red-600 underline" : "text-blue-600"}`}>
             Home
             </a>
           </Link>
-          
+          </div>
+          <div>
           <Link href="/About">
           <a className={`transition mr-4 font-bold my-auto hover:text-blue-600 hover:-translate-y-1   ${router.pathname == "/About" ? "text-red-600 underline" : "text-blue-600"}`}>
               About
               </a>
             </Link>
-
+            </div>
+            <div>
             <Link href="/Contact">
           <a className={`transition mr-4 font-bold my-auto hover:text-blue-600 hover:-translate-y-1   ${router.pathname == "/Contact" ? "text-red-600 underline" : "text-blue-600"}`}>
               Contact
               </a>
             </Link>
-
+            </div>
             
 
           { /* <Link href="/About"> THIS IS HOW WITH A BUTTON
@@ -62,5 +66,7 @@ export const Navbar : React.FC = () =>{
           </div>
 
           </nav>
+
+          </Container>
     );
 };
